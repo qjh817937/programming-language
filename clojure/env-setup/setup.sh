a73a899aa690523e940a9ac96b7da5d968a9cf82
jianshen_0 <jianshen@dev136002.sqa.cm4>
2015-04-23 21:52:19 +0800

    add env-setup

---
+++ clojure/env-setup/setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+current_dir=$(cd `dirname $0`; pwd)
+
+function download() 
+{
+    git clone --depth=1 git://github.com/richhickey/clojure.git
+    git clone --depth=1 git://github.com/richhickey/clojure-contrib.git
+}
+
+function compile() 
+{
+    cd $current_dir/clojure
+    ant clean jar
+
+    cd $current_dir/clojure-contrib
+    mvn package -Dclojure.jar=$current_dir/clojure/clojure-1.2.0-master-SNAPSHOT.jar 
+    mvn clojure:repl -Dclojure.jar=$current_dir/clojure/clojure-1.2.0-master-SNAPSHOT.jar 
+}
+
+function run1() 
+{
+    cd $current_dir
+    java -jar $current_dir/clojure/clojure-1.2.0-master-SNAPSHOT.jar $current_dir/helloworld.clj
+}
+
+function run2() 
+{
+    cd $current_dir
+    java -cp $current_dir/clojure/clojure-1.2.0-master-SNAPSHOT.jar clojure.main $current_dir/helloworld.clj
+}
+
+function run3() 
+{
+    cd $current_dir
+    export CLASSPATH=$current_dir/clojure/clojure-1.2.0-master-SNAPSHOT.jar
+    ./clojure-contrib/launchers/bash/clj-env-dir  ./helloworld.clj
+}
+
+download
+
+compile
+
+run1
+run2
+run3
